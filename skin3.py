030f4f156fc913128ad035536de9b56e8a06fa69
Himika-Mishra <55480892+Himika-Mishra@users.noreply.github.com>
2023-10-19 16:58:44 +0530

    Update skin3.py
    Normalised the counts and changed the Age Prediction model

--- skin3.py
+++ skin3.py
@@ -6,6 +6,9 @@ from PIL import Image
 from skimage import feature
 import time
 from deepface import DeepFace
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image as tf_image
+import os
 
 # Streamlit settings and styles
 st.set_page_config(page_title="Face Analysis", page_icon=":smiley:")
@@ -52,7 +55,11 @@ mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh()
 mp_drawing = mp.solutions.drawing_utils
 
-@st.cache
+# Load Model
+MODEL_PATH = 'more_data(3).h5'
+new_model = load_model(MODEL_PATH)
+
+#@st.cache
 def compute_lbp_texture(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     radius = 2
@@ -62,7 +69,7 @@ def compute_lbp_texture(image):
     lbp_hist = lbp_hist.astype('float')
     return np.sum(lbp_hist)
 
-@st.cache
+#@st.cache
 def draw_landmarks_with_flicker(image):
     results = face_mesh.process(image)
     landmarks_image = np.zeros_like(image, dtype=np.uint8)
@@ -92,7 +99,7 @@ def draw_landmarks_with_flicker(image):
     
     return blended_image
 
-@st.cache
+#@st.cache
 def count_wrinkles_and_spots(roi):
     gray_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     bilateral = cv2.bilateralFilter(gray_roi, 9, 80, 80)
@@ -111,18 +118,18 @@ def count_wrinkles_and_spots(roi):
     contours, _ = cv2.findContours(thresh1, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
     # Filter out very small contours to reduce noise
-    min_spot_area = 10
+    min_spot_area = 4
     spots = len([cnt for cnt in contours if cv2.contourArea(cnt) > min_spot_area])
     
     return wrinkles, spots
 
-@st.cache
+#@st.cache
 def count_features(image):
     wrinkles, spots = count_wrinkles_and_spots(image)
     texture = compute_lbp_texture(image)
     return wrinkles, spots, texture
 
-@st.cache
+#@st.cache
 def process_image(uploaded_image):
     image = Image.open(uploaded_image).convert("RGB")
     frame = np.array(image)
@@ -133,6 +140,23 @@ def process_image(uploaded_image):
     frame = draw_landmarks_with_flicker(frame)
 
     return frame, wrinkles, spots, texture
+
+#@st.cache
+def loadImage(filepath):
+    test_img = tf_image.load_img(filepath, target_size=(180, 180))
+    test_img = tf_image.img_to_array(test_img)
+    test_img = np.expand_dims(test_img, axis=0)
+    test_img /= 255
+    return test_img
+
+#@st.cache
+def model_predict(img_path):
+    global new_model
+    age_pred = new_model.predict(loadImage(img_path))
+    x = age_pred[0][0]
+    rounded_age_value = round(x)  # Rounds 24.56 to 25
+    age = 'About '+ str(rounded_age_value) +' years old'
+    return age
 
 # Streamlit UI
 st.markdown("<div class='big-font'>Face Analysis App</div>", unsafe_allow_html=True)
@@ -153,53 +177,43 @@ if uploaded_file:
 
     st.image(frame, channels="RGB", use_column_width=True, caption="Face landmarks.")
     
-    st.markdown(f"**Number of Wrinkles Detected:** {wrinkles}")
-    st.markdown(f"**Number of Spots Detected:** {spots}")
-    st.markdown(f"**Texture Count:** {texture}")
+    def min_max_scale(value, min_value, max_value):
+        """Scales a given value between 0 and 100 using Min-Max scaling."""
+        return (value - min_value) / (max_value - min_value) * 100
+
+    # Define some hypothetical maximum values for wrinkles, spots, and texture 
+    # based on your dataset or domain knowledge.
+    MAX_WRINKLES = 100000  # Just a placeholder value; adjust accordingly
+    MAX_SPOTS = 100000     # Just a placeholder value; adjust accordingly
+    MAX_TEXTURE = 100000   # Just a placeholder value; adjust accordingly
+    
+    # Use the min_max_scale function to scale each feature value to [0, 100]
+    scaled_wrinkles = min_max_scale(wrinkles, 0, MAX_WRINKLES)
+    scaled_spots = min_max_scale(spots, 0, MAX_SPOTS)
+    scaled_texture = min_max_scale(texture, 0, MAX_TEXTURE)
+    
+    # Display the scaled scores in Streamlit
+    st.markdown("<div class='block-container hover'>", unsafe_allow_html=True)
+    st.markdown(f"**Standardized Wrinkles Score:** {scaled_wrinkles:.2f}")
+    st.markdown(f"**Standardized Spots Score:** {scaled_spots:.2f}")
+    st.markdown(f"**Standardized Texture Score:** {scaled_texture:.2f}")
 
     st.markdown("</div>", unsafe_allow_html=True)
 
 if uploaded_file is not None:
     st.write("SKIN AGE ANALYSIS.")
+    st.write("Model Loaded Successfully!")
     # Display the uploaded image
-    st.image(uploaded_file, use_column_width=True)
-
-    # Convert the uploaded image to a format compatible with OpenCV
-    image_data = uploaded_file.read()
-    image_array = np.fromstring(image_data, np.uint8)
-    img = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-
-    # Initialize Mediapipe Face Detection
-    mp_face_detection = mp.solutions.face_detection
-    face_detection = mp_face_detection.FaceDetection(min_detection_confidence=0.2)
-
-    # Detect faces in the image
-    with mp_face_detection.FaceDetection(min_detection_confidence=0.2) as face_detection:
-        result = face_detection.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-
-    # Check if faces were detected
-    if result.detections:
-        for detection in result.detections:
-            bboxC = detection.location_data.relative_bounding_box
-            ih, iw, _ = img.shape
-            bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)
-
-            # Extract the face region
-            face_img = img[bbox[1]:bbox[1] + bbox[3], bbox[0]:bbox[0] + bbox[2]]
-
-            # Calculate skin age using DeepFace
-            try:
-                # Analyze the face image using DeepFace
-                analysis = DeepFace.analyze(face_img, actions=['age'], enforce_detection=False)
-                
-                # Loop through each face in the result
-                for face in analysis:
-                    age = face['age']
-                    st.write(f"Estimated skin age: {age} years")
-            except Exception as e:
-                st.write("Error analyzing face: " + str(e))
-    else:
-        st.write("No faces detected in the image.")
+    st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
+    st.write("")
+    st.write("Predicting...")
+    
+    # Save the uploaded file temporarily and predict
+    #with open(os.path.join("tempFile.jpg"), "wb") as f:
+     #    f.write(uploaded_file.getbuffer())
+         
+    age = model_predict(uploaded_file)
+    st.write(age)
 
 # Display some additional information
 st.info("Note: The accuracy of age estimation may vary.")
